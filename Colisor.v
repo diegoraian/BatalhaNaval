13085864b75ae4e918c82e8812943eeeeb35559d
douglas-wiliam <dougl.wil@gmail.com>
2017-07-27 00:06:26 -0300

    Saida clear do Colisor alterada de 8 para 64bits

--- Colisor.v
+++ Colisor.v
@@ -3,9 +3,8 @@
   Anotações:
   
   - Verifica o acerto de tiro na embarcação inimiga
-  
   - Exclui da memória caso a coordenada seja encontrada
-  
+  - Leva 11 pulsos de clock para percorrer toda a memoria
   
   --------------------------------------------------------
 */
@@ -19,22 +18,20 @@ module Colisor
    @param x   - coordenad x do tiro
    @param y   - coordenad y do tiro
    @param memoriaP1 - vetor de coordenadas lido da memoria do player 1
-	 @param memoriaP2 - vetor de coordenadas lido da memoria do player 2
-	 @param jogador - Jogador que atirou
+	@param memoriaP2 - vetor de coordenadas lido da memoria do player 2
+	@param jogador - Jogador que atirou
     
   */
   enable, x, y, clk, jogador, memoriaP1, memoriaP2,
   
   /*
   
-  	@param ready - passa a atribuicao de volta ao módulo que o chamou
-    @param hit - se a posicao verificada ja existe na memoria tiro eh 1, se nao conflito eh 0 
-    @param wrep1 - write enable player 1
-    @param wrep2 - write enable player 2
-    @param tiro - vetor de saída com a coordenada do tiro
-    @param addr - endereço para ler na memoria
-	  @param clear - vetor que ira substituir por zero um endereco ja atingido em memoriaP1 ou  memoriaP2
-
+	@param ready - passa a atribuicao de volta ao módulo que o chamou
+	@param hit - se a posicao verificada ja existe na memoria tiro eh 1, se nao conflito eh 0 
+	@param wrep1 - write enable player 1
+	@param wrep2 - write enable player 2
+	@param addr - endereço para ler na memoria
+	@param clear - vetor que ira substituir por zero um endereco ja atingido em memoriaP1 ou  memoriaP2
 	 
   */
   ready, hit, wrep1, wrep2, tiro, addr, clear
@@ -60,20 +57,20 @@ output reg ready;
 output reg hit;
 output reg wrep1;
 output reg wrep2;
-output reg [7:0] tiro;
 output reg [4:0] addr;
-output reg [7:0] clear;
+output reg [63:0] clear;
 
-
+/* Tiro - vetor com as coordenadasx e y do tiro */
+output reg [7:0] tiro;	// Nao ha sentido no mesmo ser um output, no entanto, como um registrador apenas, ocorre um erro de "memoria insuficiente"
 
 always @ (posedge clk or negedge enable) begin		
 	
 	if (enable == 0) begin
 		addr = 5'd0;
 	end else begin
-		if (addr == 5'd11) begin	// se endereco chegou ao ultimo
-			addr = 5'd0;						// zera o endereco
-			ready = 1'b1;						// informa que terminou a tarefa
+		if (addr == 5'd11) begin		// se o endereco eh maior que o endereco do ultimo
+			addr = 5'd0;					// zera o endereco
+			ready = 1'b1;					// informa que terminou a tarefa
 		end else begin
 			addr = addr + 5'd1;			// incrementa o endereço para verificar na memoria. Em um total de 11 pulsos de clock ele verifica a memoria inteira
 		end
@@ -91,14 +88,13 @@ always @ (posedge clk or negedge enable) begin
 	
 		if((wrep1 == 1'b1) || (wrep2 == 1'b1)) begin			// se escrita eh permitida
 			addr = addr - 5'd1;										// seta o endereco para o ultimo cujo tiro atingiu
-			clear = 8'b00000000;										// insere vetor clear na posicao de memoria a ser apagada
 			ready = 1'b1;												// informa que terminou a tarefa
 		end
 	end
 end
 
 
-/*Buscar na memoria todas os vetores e verificar se a posição ja esta ocupada */
+/*Buscar na memoria todos os vetores e verificar se a posição ja esta ocupada */
 
 always @(enable or addr) begin
 		
@@ -106,31 +102,37 @@ always @(enable or addr) begin
 		tiro[3 -:4] = x;
 		tiro[6 -:4] = y;
 		
-		if(jogador == 0) begin				// Se for o player 1
-      case (tiro[7 -:8])
+		if(jogador == 0) begin						// Se for o player 1
+			clear[63:0] = memoriaP1[63:0];		//Copia para o clear o valor da memoria
+			case (tiro[7 -:8])
 			
 				memoriaP1[10 -:8]:
 				begin
-					hit = 1'b1;					// Seta o tiro para player 1
+					clear[10-:8] = 8'b00000000;	// insere 0 na posicao de memoria a ser apagada
+					hit = 1'b1;							// Seta o tiro para player 1
 				end
 					
-        memoriaP1[18 -:8]:
+				memoriaP1[18 -:8]:
 				begin
+					clear[18-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 					
-        memoriaP1[26 -:8]:
+				memoriaP1[26 -:8]:
 				begin
+					clear[26-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 					
-        memoriaP1[34 -:8]:
+				memoriaP1[34 -:8]:
 				begin
+					clear[34-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 					
-        memoriaP1[44 -:8]:
+				memoriaP1[42 -:8]:
 				begin
+					clear[42-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 			
@@ -141,30 +143,36 @@ always @(enable or addr) begin
 			endcase
 			
 		end else if(jogador == 1) begin			// Se for o player 2
+			clear[63:0] = memoriaP2[63:0];		//Copia para o clear o valor da memoria
 			case (tiro[7 -:8])
 			
 				memoriaP2[10 -:8]:
 				begin
+					clear[10-:8] = 8'b00000000;	// insere 0 na posicao de memoria a ser apagada
 					hit = 1'b1;							// Seta o tiro para player 2
 				end
 					
-        memoriaP2[18 -:8]:
+				memoriaP2[18 -:8]:
 				begin
+					clear[18-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 					
-        memoriaP2[26 -:8]:
+				memoriaP2[26 -:8]:
 				begin
+					clear[26-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 					
-        memoriaP2[34 -:8]:
+				memoriaP2[34 -:8]:
 				begin
+					clear[34-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 					
-        memoriaP2[44 -:8]:
+				memoriaP2[42 -:8]:
 				begin
+					clear[42-:8] = 8'b00000000;
 					hit = 1'b1;
 				end
 			
@@ -180,8 +188,8 @@ always @(enable or addr) begin
 
 	else begin											// caso enable nao esteja habilitado
 		hit = 1'b0;
-    tiro[3 -:4] = 4'd0;
-    tiro[7 -:4] = 4'd0;
+		tiro[3 -:4] = 4'd0;
+		tiro[7 -:4] = 4'd0;
 	end
 end
   
